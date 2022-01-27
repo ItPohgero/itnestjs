55ea79ed0b4988ce8f90612e1f94b1a4f5130062
Wahyu Agus Arifin - IP <itpohgero@gmail.com>
2022-01-27 21:00:18 +0700

    push

--- src/posts/dto/update-post.dto.ts
+++ src/posts/dto/update-post.dto.ts
@@ -1,6 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePostDto } from './create-post.dto';
+import { IsNumberString } from 'class-validator';
 
 export class UpdatePostDto extends PartialType(CreatePostDto) {
+  @IsNumberString()
   title: string;
 }
